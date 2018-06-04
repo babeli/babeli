cc80c96a54a6e02aa5fbf564b972ea0a092af54b
hackercowboy <david@codecowboys.io>
2018-06-04 20:29:47 +0200

    added hapi

--- src/server.js
+++ src/server.js
@@ -1,47 +1,80 @@
+import Path from 'path';
 import App from './App';
 import React from 'react';
 import { StaticRouter } from 'react-router-dom';
-import express from 'express';
+import Hapi from 'hapi';
+import Inert from 'inert';
 import { renderToString } from 'react-dom/server';
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
-const server = express();
-server
-  .disable('x-powered-by')
-  .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
-  .get('/*', (req, res) => {
-    const context = {};
-    const markup = renderToString(
-      <StaticRouter context={context} location={req.url}>
-        <App />
-      </StaticRouter>
-    );
+const server = new Hapi.Server({
+  port: 4000
+});
 
-    if (context.url) {
-      res.redirect(context.url);
-    } else {
-      res.status(200).send(
-        `<!doctype html>
-    <html lang="">
-    <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta charset="utf-8" />
-        <title>Welcome to Razzle</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${assets.client.css
-          ? `<link rel="stylesheet" href="${assets.client.css}">`
-          : ''}
-        ${process.env.NODE_ENV === 'production'
-          ? `<script src="${assets.client.js}" defer></script>`
-          : `<script src="${assets.client.js}" defer crossorigin></script>`}
-    </head>
-    <body>
-        <div id="root">${markup}</div>
-    </body>
-</html>`
-      );
-    }
-  });
+(async () => {
+  try {
+    await server.register(Inert);
+    await server.route({
+      method: 'GET',
+      path: process.env.RAZZLE_PUBLIC_DIR + '/{file*}',
+      handler: {
+        directory: {
+          path: process.env.RAZZLE_PUBLIC_DIR
+        }
+      }
+    });
 
-export default server;
+    await server.route({
+      method: 'GET',
+      path: '/{path*}',
+      options: {
+        async handler(request, h) {
+          try {
+            const context = {};
+            const markup = renderToString(
+              <StaticRouter context={context} location={request.url}>
+                <App />
+              </StaticRouter>
+            );
+
+            if (context.url) {
+              return h.redirect(context.url);
+            }
+            else {
+              const template = `
+                <!doctype html>
+                <html lang="">
+                <head>
+                  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                  <meta charset="utf-8" />
+                  <title>Welcome to Razzle</title>
+                  <meta name="viewport" content="width=device-width, initial-scale=1">
+                  ${assets.client.css
+                  ? `<link rel="stylesheet" href="${assets.client.css}">`
+                  : ''}
+                  ${process.env.NODE_ENV === 'production'
+                  ? `<script src="${assets.client.js}" defer></script>`
+                  : `<script src="${assets.client.js}" defer crossorigin></script>`}
+                </head>
+                <body>
+                  <div id="root">${markup}</div>
+                </body>
+                </html>`
+
+              return h.response(template).code(200);
+            }
+          }
+          catch (err) {
+            console.log(err);
+          }
+        }
+      }
+    });
+
+    await server.start();
+  }
+  catch (err) {
+    console.log(err);
+  }
+}) ();
